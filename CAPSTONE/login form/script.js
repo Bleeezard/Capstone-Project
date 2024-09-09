aff8d44a4db02c36703e98c95788dc982574f17a
Bleeezard <ckc172002@gmail.com>
2024-09-09 18:50:52 +0800

    Add files via upload

---
+++ CAPSTONE/login form/script.js
@@ -0,0 +1,23 @@
+const search = document.querySelector(".input-group input"),
+  table_rows = document.querySelectorAll("tbody tr"),
+  table_headings = document.querySelectorAll("thead th");
+
+table_headings.forEach((head, i) => {
+  let sort_asc = true;
+  head.onclick = () => {
+    table_headings.forEach((head) => head.classList.remove("active"));
+    head.classList.add("active");
+
+    document
+      .querySelectorAll("td")
+      .forEach((td) => td.classList.remove("active"));
+    table_rows.forEach((row) => {
+      row.querySelectorAll("td")[i].classList.add("active");
+    });
+
+    head.classList.toggle("asc", sort_asc);
+    sort_asc = head.classList.contains("asc") ? false : true;
+
+    sortTable(i, sort_asc);
+  };
+});
